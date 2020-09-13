8b22108ae7d9bde9c2483951b86b1e793f5942dd
winstoncwang <congwang@live.co.uk>
2020-09-13 19:50:51 +0100

    MVC architecture

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ const path = require('path');
 const CORS = require('cors');
 const PORT = process.env.PORT || 5000;
 
-app
-	.use(CORS())
-	.use(express.static(path.join(__dirname, 'public')))
-	.get('/', (req, res) =>
-		res.sendFile(path.join(__dirname, 'public/index.html'))
-	);
+app.use(CORS()).use(express.static(path.join(__dirname, 'public')));
 
 const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
