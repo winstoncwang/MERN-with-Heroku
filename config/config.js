4998db77d634e11ea8152dac9bf7f77796780f9e
winstoncwang <congwang@live.co.uk>
2020-09-21 21:55:47 +0100

    config update to remote mongodb

--- config/config.js
+++ config/config.js
@@ -1,6 +1,9 @@
 const config = {
 	port      : process.env.PORT || 3000,
-	db        : process.env.MONGOLAB_URI || 'mongodb://localhost/api',
+	db        :
+		process.env.MONGOLAB_URI ||
+		process.env.ATLAS_URI ||
+		'mongodb://localhost/api',
 	test_port : 3001,
 	test_db   : 'mongodb://localhost/api_test'
 };
