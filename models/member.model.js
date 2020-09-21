ac720b72d25191ca5245bf1bf1767cae25ca5820
winstoncwang <congwang@live.co.uk>
2020-09-21 21:43:48 +0100

    Fixed #4
    schema for member created and working for CRUD processes

--- models/member.model.js
+++ models/member.model.js
@@ -3,19 +3,24 @@ const Schema = mongoose.Schema;
 
 const memberSchema = new Schema({
 	//_id auto generated
-	username            : { type: String, required: true, unique: true },
+	username            : {
+		type     : String,
+		required : true,
+		unique   : true,
+		trim     : true,
+		min      : 3
+	},
 	firstname           : String,
 	familyname          : String,
 	age                 : { type: Number, min: 18, max: 100 },
-	accountCreationDate : { type: Date, default: Date.now() },
-	accountUpdateDate   : { type: Date, default: Date.now() },
+	accountcreationdate : { type: Date, default: Date.now() },
+	accountupdatedate   : { type: Date, default: Date.now() },
 	dob                 : {
 		date  : { type: Number, min: 1, max: 31, required: true },
 		month : { type: Number, min: 1, max: 12, required: true },
 		year  : { type: Number, required: true }
 	},
-	password            : {},
-	membershipOf        : []
+	password            : { type: String, required: true }
 });
 
 //model
