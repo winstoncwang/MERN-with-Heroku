b3458b4d690098b784b204e99aee51f8deb4ba20
Cong Wang <congwang@live.co.uk>
2020-09-26 03:31:31 +0100

    mock test updateOne() method

--- test/mongoose.test.js
+++ test/mongoose.test.js
@@ -7,54 +7,147 @@ const Member = require('../models/member.model');
 
 describe('CRUD Mongoose', () => (
 	describe('Create API testing', () => {
-		/* save() tests */
-		it('save() successful', (done) => {
-			let newMemberObj = new Member(
-				new Member({ username: 'memberOne' }) //where username is the required field
+		let newMembObjSuccess;
+		let newMembObjFailed;
+		let ApiSuccessMock;
+		let ApiFailedMock;
+
+		before(() => {
+			newMembObjSuccess = new Member({
+				username: 'winstoncwang',
+				firstname: 'Winston',
+				familyname: 'Wang',
+				age: 13,
+				dob: {
+					date: 15,
+					month: 1,
+					year: 2007,
+				},
+				password: 'encryptedstring',
+			}); //where username is the required field
+			newMembObjFailed = new Member(
+				{ username: 'memberOne' }, //where username is the required field
 			);
 
-			let ApiMock = sinon.mock(newMemberObj);
+			ApiSuccessMock = sinon.mock(newMembObjSuccess);
+			ApiFailedMock = sinon.mock(newMembObjFailed);
 
-			let expectation = { status: true };
+			expectationSuccess = {
+				status: 200,
+				response: {
+					accountcreationdate: '2020-09-26T01:05:38.711Z',
+					accountupdatedate: '2020-09-26T01:05:38.711Z',
+					_id: '5f6e9602cbc91a464877d9e6',
+					username: 'winstoncwang',
+					firstname: 'Winston',
+					familyname: 'Wang',
+					age: 13,
+					dob: {
+						date: 15,
+						month: 1,
+						year: 2007,
+					},
+					password: 'encryptedstring',
+					__v: 0,
+				},
+			};
+			expectationFailed = {
+				status: 400,
+				error: {
+					ValidationError:
+						'dob.year: Path `dob.year` is required., dob.month: Path `dob.month` is required., dob.date: Path `dob.date` is required.',
+				},
+			};
+		});
 
-			ApiMock.expects('save').once().yields(null, expectation);
+		after(() => {
+			ApiSuccessMock.verify();
+			ApiFailedMock.verify();
+		});
 
-			newMemberObj.save((error, doc) => {
-				expect(doc.status).to.be.true;
-				ApiMock.verify();
-				ApiMock.restore();
+		/* save() tests */
+		it('save() successful', (done) => {
+			ApiSuccessMock.expects('save')
+				.once()
+				.yields(null, expectationSuccess);
+
+			newMembObjSuccess.save((error, result) => {
+				assert.strictEqual(result.status, 200, 'status code:200');
+				expect(result.response).to.have.ownProperty(
+					'accountcreationdate',
+					'2020-09-26T01:05:38.711Z',
+					'accountupdatedate',
+					'2020-09-26T01:05:38.711Z',
+					'_id',
+					'5f6e9602cbc91a464877d9e6',
+					'username',
+					'winstoncwang',
+					'firstname',
+					'Winston',
+					'familyname',
+					'Wang',
+					'age',
+					13,
+					'dob',
+					{
+						date: 15,
+						month: 1,
+						year: 2007,
+					},
+					'password',
+					'encryptedstring',
+					'__v',
+					0,
+				);
+				expect(error).to.be.null;
 				done();
 			});
+		});
+		it('save() failed', (done) => {
+			ApiFailedMock.expects('save')
+				.once()
+				.yields(expectationFailed, null);
+
+			newMembObjFailed.save((err, result) => {
+				expect(result).to.be.null;
+				assert.strictEqual(err.status, 400, 'status code:400');
+				expect(err.error).to.have.ownProperty('ValidationError');
+			});
+			done();
 		});
 	}),
 	describe('Read API testing', () => {
 		//creating mock of apimodel
 		let ApiMock;
-		let findSuccess;
-		let findFail;
-		let findByIdSuccess;
-		let findByIdFail;
+		let expectationFindSuccess;
+		let expectationFindFail;
+		let expectationFindByIdSuccess;
+		let expectationFindByIdFail;
 
 		before(() => {
-			findSuccess = { status: true, member: {} };
-			findFail = {
-				status : false,
-				error  : 'failed to gather data'
-			};
-			findByIdSuccess = {
-				_id            : 1284584,
-				firstName      : 'Bob',
-				secondName     : 'Windy',
-				age            : 24,
-				countryOfBirth : 'Canada'
-			};
-			findByIdFail = { status: false, error: 'no such user exist' };
 			ApiMock = sinon.mock(Member);
+
+			expectationFindSuccess = { status: true, member: {} };
+			expectationFindFail = {
+				status: false,
+				error: 'failed to gather data',
+			};
+			expectationFindByIdSuccess = {
+				_id: 1284584,
+				firstName: 'Bob',
+				secondName: 'Windy',
+				age: 24,
+				countryOfBirth: 'Canada',
+			};
+			expectationFindByIdFail = {
+				status: false,
+				error: 'no such user exist',
+			};
 		});
 
 		after(() => {
 			ApiMock.verify(); //verifies the expectations from first to last and restore()
-			ApiMock.restore(); //Resets the original method
+			//ApiMock.restore(); //Resets the original method
 		});
 
 		/* find() tests */
@@ -64,29 +157,26 @@ describe('CRUD Mongoose', () => (
 			ApiMock.expects('find')
 				.once()
 				.withArgs({})
-				.yields(null, findSuccess);
+				.yields(null, expectationFindSuccess);
 
 			//use model.find() to get all data
 			//CREATED MODEL FOR SCHEMA hence able to use .find() without .exec()
-			console.log(ApiMock);
 			Member.find({}, (err, result) => {
 				expect(result.status).to.be.true;
+				expect(err).to.be.null;
 				done();
 			});
 		});
 
 		it('find() failed', (done) => {
-			console.log('after verify', ApiMock);
 			ApiMock.expects('find')
 				.once()
 				.withArgs({ _id: 1234 })
-				.yields(findFail, null);
-			console.log('after new expects', ApiMock);
+				.yields(expectationFindFail, null);
+
 			Member.find({ _id: 1234 }, (err, result) => {
 				expect(err.status).to.be.not.true;
-
-				ApiMock.verify();
-				ApiMock.restore();
+				expect(result).to.be.null;
 				done();
 			});
 		});
@@ -96,8 +186,8 @@ describe('CRUD Mongoose', () => (
 			ApiMock.expects('findById')
 				.once()
 				.withArgs(1284583)
-				.yields(null, findByIdSuccess);
-			console.log(ApiMock);
+				.yields(null, expectationFindByIdSuccess);
+
 			Member.findById(1284583, (err, resultById) => {
 				expect(resultById).to.have.own.property(
 					'_id',
@@ -109,7 +199,7 @@ describe('CRUD Mongoose', () => (
 					'age',
 					24,
 					'countryOfBirth',
-					'Canada'
+					'Canada',
 				);
 				expect(err).to.be.null;
 				done();
@@ -121,7 +211,7 @@ describe('CRUD Mongoose', () => (
 			ApiMock.expects('findById')
 				.once()
 				.withArgs(1284584)
-				.yields(findByIdFail, null);
+				.yields(expectationFindByIdFail, null);
 			//mongoose callback
 			Member.findById(1284584, (err, result) => {
 				expect(result).to.be.null;
@@ -130,10 +220,76 @@ describe('CRUD Mongoose', () => (
 			});
 			done();
 		});
+	}),
+	describe('Update API test', () => {
+		let ApiMock;
+		let expectationUpdateOneFailed;
+		let expectationUpdateOneSuccess;
+
+		before(() => {
+			ApiMock = sinon.mock(Member);
+
+			expectationUpdateOneSuccess = {
+				status: 200,
+				response: { n: 1, nModified: 1, ok: 1 },
+			};
+			expectationUpdateOneFailed = {
+				status: 400,
+				DocumentNotFoundError: '$filename not found.',
+			};
+		});
+
+		after(() => {
+			ApiMock.verify();
+		});
+
+		it('updateOne() successful', (done) => {
+			ApiMock.expects('updateOne')
+				.once()
+				.withArgs({ username: 'winstoncwang' }, { $set: { age: 55 } })
+				.yields(null, expectationUpdateOneSuccess);
+
+			Member.updateOne(
+				{ username: 'winstoncwang' },
+				{ $set: { age: 55 } },
+				(error, result) => {
+					expect(result.status).to.equal(200);
+					expect(result.response).to.have.ownProperty(
+						'n',
+						1,
+						'nModified',
+						1,
+						'ok',
+						1,
+					);
+					expect(error).to.be.null;
+				},
+			);
+			done();
+		});
+
+		it('updateOne() failed', (done) => {
+			ApiMock.expects('updateOne')
+				.once()
+				.withArgs(
+					{ username: 'winstoncwang' },
+					{ $set: { age: 55, password: 'encryptedString' } },
+				)
+				.yields(expectationUpdateOneFailed, null);
+
+			Member.updateOne(
+				{ username: 'winstoncwang' },
+				{ $set: { age: 55, password: 'encryptedString' } },
+				(err, result) => {
+					expect(result).to.be.null;
+					expect(err.status).to.equal(400);
+					expect(err.response).to.have.ownProperty(
+						'DocumentNotFoundError',
+						'$filename not found.',
+					);
+				},
+				done(),
+			);
+		});
 	})
 ));
-
-/* 0: disconnected
-1: connected
-2: connecting
-3: disconnecting */
